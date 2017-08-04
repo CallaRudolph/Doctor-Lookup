659a13055664236df17403099d3cd431735d6703
Calla Rudolph <callarudolph@gmail.com>
2017-08-04 11:06:58 -0700

    add links to google and better doctor api

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('#input').click(function() {
     var symptom = $('#symptom').val();
+    $('#symptom').val("");
     $('#doctors').empty();
     $('.response').show();
     $('#doctors').show();
